190bcedf9e5295a8814562a46a24711d2e1c5dd4
Elissa Ogburn <eogburn1@umbc.edu>
2020-04-03 18:03:07 -0400

    Update APIquery to accept variables for state and county number.

--- API_Connections/Census_pop.py
+++ API_Connections/Census_pop.py
@@ -7,11 +7,19 @@ import requests
 #allows us to store results of API call cleanly
 import json
 
+state = '01'
+county = '001'
+
+APIquery = f"https://api.census.gov/data/2010/dec/pl?get=P001001,NAME&for=block%20group:*&in=state:{state}%20county:{county}&key=78ae8c422513eb7551e52f2adf65ee6b51847b9d"
 #construct the API call we will use
-APIquery = "https://api.census.gov/data/2010/dec/pl?get=P001001,NAME&for=block%20group:*&in=state:01%20county:073&key=78ae8c422513eb7551e52f2adf65ee6b51847b9d"
+#APIquery = "https://api.census.gov/data/2010/dec/pl?get=P003001,NAME&for=block%20group:*&in=state:01%20county:073&key=78ae8c422513eb7551e52f2adf65ee6b51847b9d"
 
 response = requests.get(APIquery)
 formatResponse = json.loads(response.text)[1:]
+
+
+
+
 
 #store the response in a dataframe
 data_results = pd.DataFrame(columns=['Population',' Name',' State',' County',' Tract',' Block_Group'], data=formatResponse)

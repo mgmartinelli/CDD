7fc7ea3270c62da014dc70e92f678d79df611e01
Rohan <rahuja2@umbc.edu>
2020-05-04 02:09:24 -0400

    Adjusted settings

--- map/views.py
+++ map/views.py
@@ -7,19 +7,22 @@ import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 # Homepage view
 def home_view(request, *args, **kwargs):
 	return render(request, "home.html")
 
 # Map view of the districts
 def map_view(request, state, *args, **kwargs):
-	f = open('static/state_locations/state-locations.json') 
-	data = json.load(f) 
+    url = 'static/state_locations/state-locations.json'
+    f = open(url) 
+    data = json.load(f) 
 
-	lat = data[state]["Lat"]
-	lon = data[state]["Lon"]
+    lat = data[state]["Lat"]
+    lon = data[state]["Lon"]
 
-	return render(request, "map_view.html", {'state': state, 'lat': lat, 'lon': lon})
+    return render(request, "map_view.html", {'state': state, 'lat': lat, 'lon': lon})
 
 
 # Rest API to host JSON data for each state
@@ -28,8 +31,7 @@ class RestDashboardDistrictData(APIView):
     authentication_classes = []
     permission_classes = []
 
-    def get(self, request, state, format=None):
-        
+    def get(self, request, state, format=None):      
         json_response = []
         filename = f'static/results/{state}_Results.json'
         with open(filename, 'r') as f:
